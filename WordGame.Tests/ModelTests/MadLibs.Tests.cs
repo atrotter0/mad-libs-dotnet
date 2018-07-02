36a62e5bc924e7b8a89cd5df3bfa5b430c324d7a
Abel Trotter and Ryan Putman <abel@>
2018-07-02 11:00:31 -0700

    add test for getter and setters

--- WordGame.Tests/ModelTests/MadLibs.Tests.cs
+++ WordGame.Tests/ModelTests/MadLibs.Tests.cs
@@ -1,7 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
-using WordGame;
+using WordGame.Models;
 
 namespace WordGame.Tests
 {
@@ -9,10 +9,27 @@ namespace WordGame.Tests
     public class MadLibsTest
     {
         [TestMethod]
-        public void GetSetOccupation_GetsSetsOccupation_True()
+        public void GetSetOccupation_GetsSetsOccupation_String()
         {
-            // MadLibs newGame = new MadLibs();
-            // Assert.AreEqual(true, newGame.GetOccupation());
+            MadLibs newGame = new MadLibs();
+            newGame.SetOccupation("Farmer");
+            Assert.AreEqual("Farmer", newGame.GetOccupation());
+        }
+
+        [TestMethod]
+        public void GetSetNounOne_GetSetsNounOne_String()
+        {
+            MadLibs newGame = new MadLibs();
+            newGame.SetNounOne("Gizmo");
+            Assert.AreEqual("Gizmo", newGame.GetNounOne());
+        }
+
+        [TestMethod]
+        public void GetSetAdjectiveOne_GetSetsAdjectiveOne_String()
+        {
+            MadLibs newGame = new MadLibs();
+            newGame.SetAdjectiveOne("run");
+            Assert.AreEqual("run", newGame.GetAdjectiveOne());
         }
     }
 }
