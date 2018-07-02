aa1ae45cdb28bcaed3bb2781fda6cbb91e37d25d
Abel Trotter and Ryan Putman <abel@>
2018-07-02 08:22:03 -0700

    Add initial project outline

---
+++ WordGame/Models/MadLibs.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace WordGame
+{
+    public class MadLibs
+    {
+        public bool DoIPass()
+        {
+            return true;
+        }
+    }
+}
